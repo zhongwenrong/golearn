72fa986a66a8610efbbded93b3052a5828eb4a0c
zhongwenrong_qhwd <704184317@qq.com>
2018-03-30 14:21:44 +0800

    add

--- web.go
+++ web.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", handler)
-	http.ListenAndServe("localhost:8080", nil)
+	http.ListenAndServe("localhost:8888", nil)
 }
 func handler(rw http.ResponseWriter, req *http.Request) {
 	req.ParseForm()
